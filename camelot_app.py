6c98572e1c540f3cf5eb4bd6cf4da4e1646fda58
Sergio Sánchez Zavala <chekos.data@gmail.com>
2020-01-22 11:52:15 -0800

    Update camelot_app.py

--- camelot_app.py
+++ camelot_app.py
@@ -1,10 +1,16 @@
 import streamlit as st
+import camelot
 
 def main():
     """Streamlit app testing camelot.py"""
 
-    st.write("Hello!")
+    st.write("# Testing Camelot on Streamlit!")
 
+    uploaded_file = st.file_uploader(label="Upload PDF with tables", type="pdf")
+    tables = camelot.read_pdf(uploaded_file)
+    
+    for table in tables:
+      st.write(table.df)
 
 if __name__=="__main__":
     main()
