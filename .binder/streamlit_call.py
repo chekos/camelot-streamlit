f1694a29052284c2d069fc73c5c9974ea9cd4850
chekos <chekos.data@gmail.com>
2020-01-22 10:54:45 -0800

    try direct url

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    Popen(["streamlit", "run", "https://github.com/chekos/camelot-streamlit/blob/master/camelot_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(["streamlit", "run camelot_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
