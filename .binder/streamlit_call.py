e885c86a082c41728721be06b1ea3e1fed6f3489
chekos <chekos@tacosdedatos.com>
2020-01-23 09:24:52 -0800

    blackened code

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -3,4 +3,12 @@ from subprocess import Popen
 # app name must be last arg
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    Popen(["streamlit", "run", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False", "camelot_app.py"])
+    Popen(
+        [
+            "streamlit",
+            "run",
+            "--browser.serverAddress=0.0.0.0",
+            "--server.enableCORS=False",
+            "camelot_app.py",
+        ]
+    )
--- camelot_app.py
+++ camelot_app.py
@@ -1,10 +1,27 @@
 import streamlit as st
 import camelot
 import base64
+import uuid
+
 
 def process_pdf(pdf_bytes):
-  with open("tmp.pdf", "wb") as file:
-    file.write(pdf_bytes.read())
+    with open("tmp.pdf", "wb") as file:
+        file.write(pdf_bytes.read())
+
+
+def display_download_options(df):
+    """Receives a dataframe and displays 2 ways to download the data."""
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+
+    # href (save file) way
+    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+    st.markdown(href, unsafe_allow_html=True)
+
+    # Copy and paste way
+    if st.checkbox("Copy + paste?", key=uuid.uuid1()):
+        st.text_area(csv)
+
 
 def main():
     """Streamlit app testing camelot.py"""
@@ -13,21 +30,17 @@ def main():
 
     uploaded_file = st.file_uploader(label="Upload PDF with tables", type="pdf")
     if uploaded_file is not None:
-      process_pdf(uploaded_file)
-      tables = camelot.read_pdf("tmp.pdf", pages="all")
-    
-      for table in tables:
-        st.write(table.df)
-        df = table.df
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-        st.markdown(href, unsafe_allow_html=True)
-        if st.button("wanna just copy + paste?"):
-          st.text_area(csv)
+        process_pdf(uploaded_file)
+        tables = camelot.read_pdf("tmp.pdf", pages="all")
+
+        for table in tables:
+            df = table.df
+            st.write(df)
+            display_download_options(df)
 
     else:
-      st.write("waiting")
+        st.write("Upload a PDF file with some tables in it.")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
